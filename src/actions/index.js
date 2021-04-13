a037f54fc034da561700e0a7a7916883354f6bf5
kandari012 <73176498+kandari012@users.noreply.github.com>
2021-04-13 16:24:42 +0530

    adding actions

---
+++ src/actions/index.js
@@ -0,0 +1,5 @@
+//in code a ation is simply a JS object where we have to pass the type and can also pass any other data
+// it is a way of telling the store that i want to do someting
+{
+  type: "INCREASE_COUNT"; //will twll what action need to do
+}
