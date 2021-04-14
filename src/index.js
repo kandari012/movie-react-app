eeef0784bf2a171a2bbc54b84d68e65ec317fa89
kandari012 <73176498+kandari012@users.noreply.github.com>
2021-04-14 15:38:26 +0530

    creating the store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import "./index.css";
+import App from "./components/App";
+import movies from "./reducers/index";
 
-
+const store = createStore(movies); //creating store will pass reducer as argument
+console.log("store", store); //object with some properties
+console.log("state", store.getState()); // getting default state from reducer
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
