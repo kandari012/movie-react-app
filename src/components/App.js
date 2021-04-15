9e6423420f53f6a925e89a15ae1092d447f45868
kandari012 <73176498+kandari012@users.noreply.github.com>
2021-04-15 16:20:57 +0530

    subscribing to the store

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,50 @@
+import React from "react";
 import Navbar from "./Navbar";
 import MovieCard from "./MovieCard";
+
 import { data } from "../data";
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="main">
-        <div className="tabs">
-          <div className="tab">Movies</div>
-          <div className="tab">Favourites</div>
-        </div>
-        <div className="list">
-          {data.map((movie, index) => {
-            // willgive index of each entry
-            return <MovieCard movie={movie} key={`movies-${index}`} />;
-          })}
+class App extends React.Component {
+  componentDidMount() {
+    const { store } = this.props;
+
+    // will be called when our state changes subscribing to the store chnages
+    // after dispatched it will be caleed imidiately
+    store.subscribe(() => {
+      console.log("updated");
+      this.forceUpdate(); // forcefullu update the comp as after state change no rerener so no chnages showing on the UI
+    });
+
+    //make api call
+    //dispatch action
+    store.dispatch({
+      // will update state in store
+      type: "ADD_MOVIES",
+      movies: data,
+    });
+    console.log("state", store.getState());
+  }
+
+  render() {
+    console.log("render");
+    const movies = this.props.store.getState();
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="main">
+          <div className="tabs">
+            <div className="tab">Movies</div>
+            <div className="tab">Favourites</div>
+          </div>
+          <div className="list">
+            {movies.map((movie, index) => {
+              // willgive index of each entry
+              return <MovieCard movie={movie} key={`movies-${index}`} />;
+            })}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -9,17 +9,18 @@ const store = createStore(movies); //creating store will pass reducer as argumen
 console.log("store", store); //object with some properties
 console.log("before state", store.getState()); // getting default state from reducer
 
-store.dispatch({
-  // takes argument as action object
-  //action will be pashed to movies reducers
-  type: "ADD_MOVIES",
-  movies: [{ name: "superman" }],
-});
+// store.dispatch({
+//   // takes argument as action object
+//   //action will be pashed to movies reducers
+//   type: "ADD_MOVIES",
+//   movies: [{ name: "superman" }],
+// });
 
 console.log("after state", store.getState());
+// passing store as a prop to app
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App store={store} />
   </React.StrictMode>,
   document.getElementById("root")
 );
